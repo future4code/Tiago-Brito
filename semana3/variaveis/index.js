ece89dc91c74bc0ed0e6a075965d2b60f016521d
Tiagopb <tiago000@outlook.com>
2021-04-19 16:12:09 -0300

    reposta de ex. javascript

---
+++ semana3/variaveis/index.js
@@ -0,0 +1,36 @@
+/*const nome = "tiago"
+let idade = 33
+
+console.log(nome)
+console.log(idade)
+idade = 33
+prompt ("qual seu nome")
+console.log(typeof idade)
+console.log(typeof nome)*/
+/*O typeof leu strings que representam textos e number que representa numeros*/
+
+//console.log(typeof nome)
+//console.log(typeof idade)
+/* o typeof leu strings que representam textos*/
+
+/*const nome = prompt("Seu nome")
+let idade = prompt("Sua idade")
+console.log(nome)
+console.log(idade)
+console.log("Ola " + nome + " voce tem " + idade + " anos")*/
+
+/*const nome = prompt("Qual seu nome")
+let idade = prompt("qual sua idade")
+let ano = prompt("qual seu ano de nascimento")
+const cor = prompt("qual sua cor favorita")
+const pet = prompt("qual nome do seu pet")
+
+console.log("Resposta: " + nome + "")
+console.log("Resposta: " + idade +"")
+console.log("Resposta: " + ano +"")
+console.log("Resposta: " + cor +"")
+console.log("Resposta: " + pet +"")*/
+
+const meuArray = ["Lasanha","Feijoada","Pizza","Hamburguer","pudim"]
+
+console.log("Essas são as minhas comidas preferidas: " +meuArray+"")

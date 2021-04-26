d4ccd476a7574c74a27f6697000ca4682ac91fcf
Tiagopb <tiago000@outlook.com>
2021-04-26 16:26:39 -0300

    primeira entrega

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,94 @@
+// --- EXERCÍCIO 1 ------------------------------------------------------------------
+// a) Sera impresso os valores 10 e 50
+// b) Nada pois sem o console nao e possivel visualizar os valores da funcao.
+
+/*function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2))
+minhaFuncao(10)*/
+
+// --- EXERCÍCIO 2 ------------------------------------------------------------------
+/* a) A saida sera Darvas e Caio pois se i e igual a 0 e  i for menor que 2 sera incrementado mais 1 
+ate i chegar a 2 e com isso sera mostrado as primeiras posicoes do array o e 1.*/ 
+/*b) A saida sera Amanda e Caio pois a constante busca a duas primeiras posicoes 0 e 1*/
+
+// --- EXERCÍCIO 3 ------------------------------------------------------------------
+
+/* metodo = (array) => {
+    let arrayFinal = [];
+  
+    for (let x of array) {
+          if (x % 2 === 0) {
+          arrayFinal.push(x * x)
+          }
+    }
+    console.log(arrayFinal(15));  
+    return arrayFinal;
+   
+  }*/
+// --- EXERCÍCIO 4 ------------------------------------------------------------------
+/*a)function dados(){
+    console.log('Eu sou Tiago, tenho 33 anos, moro em Sao Paulo e sou estudante')
+    }
+    dados()*/
+
+  /*b)  function dados(nome, idade, cidade, estudante){
+        nome = prompt ('Qual seu nome?')
+        idade = Number(prompt("Qual sua idade"))
+        cidade = prompt("Qual sua cidade")
+        estudante = Boolean (prompt('Voce e estudante [sim] ou [nao]'))
+     if (profissao === 'sim'){
+          
+     }
+     else{
+         (profissao = 'nao')
+     }
+let profissao = []
+        console.log ('Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e ${estudante} sou estudante')
+        }
+
+        dados()*/
+    
+
+// --- EXERCÍCIO 5 ------------------------------------------------------------------
+/*function somaDoisNumeros(num1, num2){
+    const soma = num1 + num2
+    return soma
+}
+
+const resultado = somaDoisNumeros(2, 3)
+console.log(resultado)*/
+
+
+// --- EXERCÍCIO 6 ------------------------------------------------------------------
+
+/*function tamanhoArray(){
+
+}
+const arrayexe = [10,23,45,78,90,52,35,67,84,22]
+const tamanhoArray = tamanhoArray(arrayexe)
+
+console.log (arrayexe)*/
+
+//b) ------------------------------
+/*function recebeNumero(numero){
+    if (numero%2 === 0){
+        console.log(true)
+    }else{
+        console.log(false)
+    }
+}
+recebeNumero(6)*/
+
+// c) function justEven(array){
+   /* let count = 0
+for(let num of Array){
+    if (num % 2 === 0){
+        count+=1
+    }
+   }
+   console.log(count)
+}
+justEven(array)*/

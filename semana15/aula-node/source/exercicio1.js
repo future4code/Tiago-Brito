4fad821b98056f06b1afbcd56227edd5c465e26d
Tiagopb <tiago000@outlook.com>
2021-07-12 17:06:19 -0300

    entrega de tarefa

---
+++ semana15/aula-node/source/exercicio1.js
@@ -0,0 +1,10 @@
+//a)
+
+//b)
+const nome = String(process.argv[2] )
+const age = Number(process.argv[3])
+
+console.log(`Olá, ${nome}! Você tem ${age} anos.`)
+
+//c)
+console.log(`Olá, ${nome}! Você tem ${age} anos. Em sete anos você terá ${age+7}`)

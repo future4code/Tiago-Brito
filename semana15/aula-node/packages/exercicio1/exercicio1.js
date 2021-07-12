760a4a596c510a388fe75015b211fa8c6093996c
Tiagopb <tiago000@outlook.com>
2021-07-12 17:35:34 -0300

    exercicio 4 realizado

--- semana15/aula-node/packages/exercicio1/exercicio1.js
+++ semana15/aula-node/packages/exercicio1/exercicio1.js
@@ -1,10 +1,12 @@
 //a)
 
 //b)
-const nome = String(process.argv[2] )
+const nome = String(process.argv[2])
 const age = Number(process.argv[3])
 
 console.log(`Olá, ${nome}! Você tem ${age} anos.`)
 
 //c)
 console.log(`Olá, ${nome}! Você tem ${age} anos. Em sete anos você terá ${age+7}`)
+
+

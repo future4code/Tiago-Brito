262a685399cea7657d4772aeccf3bbe0c4a8f3a1
Tiagopb <tiago000@outlook.com>
2021-07-14 17:56:21 -0300

    atulaizado o endpoint 4

--- semana15/aula43/template-express/src/endpoints/postCountryById.ts
+++ semana15/aula43/template-express/src/endpoints/postCountryById.ts
@@ -1,37 +1,25 @@
-import { Request, Response } from "express"
-import { countries } from "../data"
-import { country } from "../types"
+import { Request, Response } from "express";
+import { countries } from "../data";
+import { country } from "../types";
 
-export const postCountryById= (
-   req: Request,
-   res: Response
-): void => {
-   const { name, capital } = req.body;
-   let errorCode: number = 400;
- 
-   try {
-     const countryIndex: number = countries.findIndex(
-       (country) => country.id === Number(req.params.id)
-     );
- 
-     if (countryIndex === -1) {
-       errorCode = 404;
-       throw new Error("Id doesn't exist");
-     }
- 
-     if (!name && !capital) {
-       throw new Error("Invalid Parameters");
-     }
- 
-     if (name) {
-       countries[countryIndex].name = name;
-     }
- 
-     if (capital) {
-       countries[countryIndex].capital = capital;
-     }
-     res.status(200).send("Country successfully updated");
-   } catch (error) {
-     res.status(errorCode).send(error.message);
-   }
- };
+export const postCountryById = (req: Request, res: Response): void => {
+  const { name, capital } = req.body;
+  const data = { name, capital };
+  console.log(req.body);
+
+  const result: country | undefined = countries.find(
+    (country) => country.id === Number(req.params.id)
+  );
+
+  console.log(result)
+
+  const alterar = {
+    // id: result?.id,
+    name: data.name,
+    capital: data.capital,
+    // continent: result?.continent,
+  };
+
+  console.log(alterar);
+  res.status(200).send(alterar);
+};

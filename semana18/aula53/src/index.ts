f172ccffc60fd4ccfc6057fb0412617e57cc4af2
Tiagopb <tiago000@outlook.com>
2021-08-03 18:51:43 -0300

    polimorfismo ate o 3

--- semana18/aula53/src/index.ts
+++ semana18/aula53/src/index.ts
@@ -117,3 +117,49 @@ const newTiago = new User("1","tiago@gmail.com","Tiago","123456");*/
   }
   
   console.log(client)
+
+
+export abstract class Place {
+  constructor(protected cep: string) {}
+
+	public getCep(): string {
+		return this.cep;
+  }
+}
+
+//const newPlace = new Place("06618000")
+  
+export class Residence extends Place {
+    constructor(
+      protected residentsQuantity: number,
+      // Refere-se ao número de moradores da casa
+  
+      cep: string
+    ) {
+      super(cep);
+    }
+  }
+
+  export class Commerce extends Place {
+    constructor(
+      protected floorsQuantity: number,
+      // Refere-se à quantidade de andares do lugar
+  
+      cep: string
+    ) {
+      super(cep);
+    }
+  }
+
+  export class Industry extends Place {
+    constructor(
+      protected machinesQuantity: number, 
+      // Refere-se à quantidade de máquinas do local 
+      
+      cep: string
+          ) {
+          super(cep);
+    }
+  }
+
+  
